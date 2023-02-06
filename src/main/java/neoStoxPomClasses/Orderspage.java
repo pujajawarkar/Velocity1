42211bf289201bf200c25f65631fbcbe0751ed34
Puja <pujajawarkar25@gmail.com>
2023-02-06 15:50:11 +0530

    new method of order page pom class

--- src/main/java/neoStoxPomClasses/Orderspage.java
+++ src/main/java/neoStoxPomClasses/Orderspage.java
@@ -5,11 +5,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 /////this class is created just for sake of understanding git branch creation and add this class to branch1
-public class Orderspage {
+public class Orderspage 
+{
 @FindBy(xpath = "(//a[text()='Sign In'])[1]") private WebElement SignInButton;//1
 	
 	public Orderspage(WebDriver driver )//2
 	{
 		PageFactory.initElements( driver, this);
+	}
+	
+	public void method( )
+	{
+		System.out.println("new method added");
 	}
 }
